b4af2303fcb8bb7517b5c7b301a377f60a7187c8
Rookid21 <steven772379@Outlook.com>
2026-06-07 18:27:18 -0700

    infrastructure code and documentation

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-# aws-infra-bootstrap
+# Secure AWS GitOps CI/CD Pipeline 
+
+[![Terraform](https://img.shields.io/badge/Terraform-7B42BC?style=for-the-badge&logo=terraform&logoColor=white)](https://www.terraform.io/)
+[![AWS](https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white)](https://aws.amazon.com/)
+[![GitHub Actions](https://img.shields.io/badge/github%20actions-%232088FF.svg?style=for-the-badge&logo=github-actions&logoColor=white)](https://github.com/features/actions)
+[![Security-Standard](https://img.shields.io/badge/IAM-Zero%20Trust%20OIDC-red?style=for-the-badge)](https://aws.amazon.com/iam/)
+
+
+### The Cloud Architecture
+The project is to achieve a centralized state management utilizing AWS over local machines, providing strong encryption, collaboration, and security through IAM, KSM, and no local keys. 
+
+
 AWS Initial Infrastructure Setup
 
-- AWS OIDC Identity Provider
-- IAM Roles for GitHub Actions
-- S3/DynamoDB setup
+## Terraform AWS Implementation Deep Dive 
+
+### 🔑 AWS KMS
+- Deploys an isolated KMS key resource giving access to data.
+- Provides server side encryption across S3 backend, utilizing a 30 day key rotation, and provides better security then the regular AES256 encryption at rest. Beneifts like key rotation, aduiting, and access controls. 
+
+
+### 🪣 AWS S3 Bucket 
+- Created a helmcove-tf-state-backend bucket specifically for terraform.tf state files
+- Enable version control for backups and resolving any potential corruption errors 
+- S3 bucket ACL preventing public access
+- Bucket-Key to cache KMS data keys saving costs
+
+
+### 🆔 AWS IAM
+- OIDC identity, not more AWS Acess Keys or Secret Keys inside code
+- Restricts only to my repo `repo:Rookid21/aws-infra-bootstrap:*`
+
+
+### GitOps Pipeline
+
+
+
+
+
+- tfstate file in S3 Bucket
+![alt text](image.png)
+
+- Secure through KMS
+![alt text](image-1.png)
+
+- IAM role
+![alt text](image-2.png)
+
+- DynamoDB to avoid overlap corruption 
+![alt text](image-3.png)
---
+++ bootstrap/main.tf
@@ -0,0 +1,135 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "helmcove-tf-state-backend"
+    key            = "bootstrap/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "helmcove-tf-state-locks"
+    kms_key_id     = "arn:aws:kms:us-west-2:670523234679:key/6d626b57-ff5c-4122-985e-a91b29f25cef"
+  }
+}
+
+#Adding aws as my provider, access key and secret is applied using "aws configure" cli
+provider "aws" {
+  region = "us-west-2"
+}
+
+#Create KMS to manage keys rotations and encryption 
+resource "aws_kms_key" "tf_state_key" {
+  description             = "KMS Key for encrypting Terraform remote state"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true 
+}
+
+# Create an alias for KSM key or else it would look like 1231vfeg-1243124
+resource "aws_kms_alias" "tf_state_key_alias" {
+  name          = "alias/helmcove-tf-state-key"
+  target_key_id = aws_kms_key.tf_state_key.key_id
+}
+
+# S3 Bucket for Terraform State
+resource "aws_s3_bucket" "tf_state" {
+  bucket        = "helmcove-tf-state-backend"
+  force_destroy = false
+
+  lifecycle {
+    prevent_destroy = true 
+  }
+}
+
+# Enable Versioning so we can recover from accidental state corruption
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enforce Server-Side Encryption using our Customer Managed KMS Key
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encrypt" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.tf_state_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+    bucket_key_enabled = true 
+  }
+}
+
+# Explicitly Block All Public Access (S3 Guardrail)
+resource "aws_s3_bucket_public_access_block" "tf_state_acl_block" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# DynamoDB Set up
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "helmcove-tf-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S" 
+  }
+}
+
+# Establish trust with GitHub OpenID Connect Identity Provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+}
+
+# Strict Trust Policy allowing only my specific repository to use this role
+data "aws_iam_policy_document" "github_oidc_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      # ENTERPRISE SECURITY: Scope down precisely to your GitHub environment
+      values = ["repo:Rookid21/aws-infra-bootstrap:*"]
+    }
+  }
+}
+
+# The IAM Role GitHub Actions will use to deploy any resources 
+resource "aws_iam_role" "github_actions_role" {
+  name               = "github-actions-infrastructure-role"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_trust.json
+}
+
+#Debugging Stuff
+output "github_actions_role_arn" {
+  value       = aws_iam_role.github_actions_role.arn
+  description = "Provide this ARN to your GitHub Actions workflow configuration"
+}
+
+output "kms_key_arn" {
+  value       = aws_kms_key.tf_state_key.arn
+  description = "The ARN of the Customer Managed KMS Key encrypting the state bucket"
+}
